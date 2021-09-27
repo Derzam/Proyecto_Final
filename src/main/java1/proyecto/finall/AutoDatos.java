ee997e8567d75a279ddb93996e19c854b618751c
Derzam <derlymoreira192@gmail.com>
2021-09-27 17:26:50 -0500

    PROYECTO FINAL

---
+++ src/main/java1/proyecto/finall/AutoDatos.java
@@ -0,0 +1,112 @@
+package proyecto.finall;
+
+import java.time.LocalDate;
+
+public class AutoDatos {
+	
+	private String Placa;
+	private String Modelo;
+	private String Marca;
+	private String anioDeFabricacion;
+	private String paisDeFabricacion;
+	private double cilindraje;
+	private String precio;
+	private String estado;
+	private String cedula;
+	private String numeroDePuertas;
+	private String pesoSoportado;
+	private String fechaDeEntrega;
+		
+	
+	public String getPlaca() {
+		return Placa;
+	}
+	public void setPlaca(String placa) {
+		Placa = placa;
+	}
+	public String getModelo() {
+		return Modelo;
+	}
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+	public String getMarca() {
+		return Marca;
+	}
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+	public String getAnioDeFabricacion() {
+		return anioDeFabricacion;
+	}
+	public void setAnioDeFabricacion(String anioDeFabricacion) {
+		this.anioDeFabricacion = anioDeFabricacion;
+	}
+	public String getPaisDeFabricacion() {
+		return paisDeFabricacion;
+	}
+	public void setPaisDeFabricacion(String paisDeFabricacion) {
+		this.paisDeFabricacion = paisDeFabricacion;
+	}
+	public double getCilindraje() {
+		return cilindraje;
+	}
+	public void setCilindraje(double cilindraje) {
+		this.cilindraje = cilindraje;
+	}
+	public String getPrecio() {
+		return precio;
+	}
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNumeroDePuertas() {
+		return numeroDePuertas;
+	}
+	public void setNumeroDePuertas(String numeroDePuertas) {
+		this.numeroDePuertas = numeroDePuertas;
+	}
+	public String getPesoSoportado() {
+		return pesoSoportado;
+	}
+	public void setPesoSoportado(String pesoSoportado) {
+		this.pesoSoportado = pesoSoportado;
+	}
+	public String getFechaDeEntrega() {
+		return fechaDeEntrega;
+	}
+	public void setFechaDeEntrega(String fechaDeEntrega) {
+		this.fechaDeEntrega = fechaDeEntrega;
+	}
+	public void setFechaDeEntrega(LocalDate diaPlazo) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setTiempoLectura(int tiempo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+		
+		
+	
+		
+	}
+	
+	
+
+	
+
+	
